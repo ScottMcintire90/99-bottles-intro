815f1f98374af77713ae3b51e5dc03cfe05d5b07
Scott McIntire <student@epicodus.com>
2016-03-02 19:33:19 -0800

    showing all verses except for the final verse of 1 beer

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var beerBottles = function(myNumber) {
   for (myNumber; myNumber > 1; myNumber--) {
-    $("#lyrics").append(myNumber + " bottles of beer on the wall," + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + myNumber + " bottles of beer on the wall.");
+    $("#lyrics").append(myNumber + " bottles of beer on the wall, " + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + (myNumber - 1) + " bottles of beer on the wall." + '<br/>' + '<br/>');
 
   };
 };
