6b3b37d0e137cdffb6476cef0e7155ebbddcc495
Scott McIntire <student@epicodus.com>
2016-03-02 19:51:11 -0800

    alter javascript code to exclude 1 beer from if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,13 @@ var beerBottles = function(myNumber) {
   for (myNumber; myNumber > 2; myNumber--) {
     $("#lyrics").append(myNumber + " bottles of beer on the wall, " + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + (myNumber - 1) + " bottles of beer on the wall." + '<br/>' + '<br/>');
   };
-  if (myNumber = 2) {
+  if (myNumber === 2) {
     $("#lyrics").append("2 bottles of beer on the wall, 2 bottles of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
-  } else if (myNumber = 1){
-    $("#lyrics").append("1 bottle of beer on the wall, 1 bottle of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
-  }
+  } else {
+  };
+    $("#lyrics").append('<br/>' + '<br/>' + "1 bottle of beer on the wall, 1 bottle of beer..." + '<br/>'+ "take one down, pass it around, no more bottles of beer on the wall.");
 };
+
 
 
 $(document).ready(function() {
