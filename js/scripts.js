9a9d37e10d6d388d56cdb90756c77ccd88609242
Scott McIntire <student@epicodus.com>
2016-03-02 19:31:14 -0800

    return first verse of number of beers on the wall that the user chose

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
+var beerBottles = function(myNumber) {
+  for (myNumber; myNumber > 1; myNumber--) {
+    $("#lyrics").append(myNumber + " bottles of beer on the wall," + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + myNumber + " bottles of beer on the wall.");
 
-
+  };
+};
 
 
 $(document).ready(function() {
-  $("#clickme").click(function(event) {
+  $("form#99bottles").submit(function(event) {
     var myNumber = parseInt($("input#myNumber").val());
-      
+    var bottles = beerBottles(myNumber);
+
   event.preventDefault();
   });
 });
