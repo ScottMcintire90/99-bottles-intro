2051feaf1c7a8b786b53983d67edbc211aef556e
Scott McIntire <student@epicodus.com>
2016-03-02 19:39:15 -0800

    make all verses work with 1 or 2 bottles of beer on the wall

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var beerBottles = function(myNumber) {
-  for (myNumber; myNumber > 1; myNumber--) {
+  for (myNumber; myNumber > 2; myNumber--) {
     $("#lyrics").append(myNumber + " bottles of beer on the wall, " + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + (myNumber - 1) + " bottles of beer on the wall." + '<br/>' + '<br/>');
-
   };
+  if (myNumber = 2) {
+    $("#lyrics").append("2 bottles of beer on the wall, 2 bottles of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
+  } else if (myNumber = 1){
+    $("#lyrics").append("1 bottle of beer on the wall, 1 bottle of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
+  }
 };
 
 
