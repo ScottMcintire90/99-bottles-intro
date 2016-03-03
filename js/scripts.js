0c9afa8f441d56b1c9cef1c5e5496cc36ce46a2c
Scott McIntire <student@epicodus.com>
2016-03-02 20:31:59 -0800

    remove jquery from the javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 var beerBottles = function(myNumber) {
   for (myNumber; myNumber > 2; myNumber--) {
-    $("#lyrics").append(myNumber + " bottles of beer on the wall, " + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + (myNumber - 1) + " bottles of beer on the wall." + '<br/>' + '<br/>');
+    return (myNumber + " bottles of beer on the wall, " + myNumber + " bottles of beer..." + '<br/>' + "take one down, pass it around, " + (myNumber - 1) + " bottles of beer on the wall." + '<br/>' + '<br/>');
   };
   if (myNumber === 2) {
-    $("#lyrics").append("2 bottles of beer on the wall, 2 bottles of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
+    return ("2 bottles of beer on the wall, 2 bottles of beer..." + '<br/>'+ "take one down, pass it around, 1 bottle of beer on the wall.");
   } else {
   };
-    $("#lyrics").append('<br/>' + '<br/>' + "1 bottle of beer on the wall, 1 bottle of beer..." + '<br/>'+ "take one down, pass it around, no more bottles of beer on the wall.");
+    return ('<br/>' + '<br/>' + "1 bottle of beer on the wall, 1 bottle of beer..." + '<br/>'+ "take one down, pass it around, no more bottles of beer on the wall.");
 };
-
 
 
 $(document).ready(function() {
   $("form#99bottles").submit(function(event) {
     var myNumber = parseInt($("input#myNumber").val());
+
+    for (myNumber; myNumber > 0; myNumber--) {
     var bottles = beerBottles(myNumber);
+
+    $("#lyrics").append(bottles);
+
+  };
 
   event.preventDefault();
   });
